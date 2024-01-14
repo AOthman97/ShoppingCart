1ee49fee8071fad845e5d6a62aaf8a2e5a978c78
Ahmed Othman <AhmedOthman97@outlook.com>
2024-01-14 22:17:37 +0300

    Added product controller and it's index, also started adding datatables and sweet alerts

---
+++ ShoppingCart.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.DataAccess.Interfaces;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private IWebHostEnvironment _hostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult GetProducts()
+        {
+            // Does the "includeProperties" here means to also get the category for each product ?
+            var products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return Json(new { productsList = products });
+        }
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ ShoppingCart.Web/wwwroot/js/Product.js
@@ -0,0 +1,39 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    dataTable = $('#productsTable').DataTable({
+        "ajax": { "url": "/Admin/Product/AllProducts" },
+        "columns": [
+            { "data": "name" },
+            { "data": "description" },
+            { "data": "price" },
+            { "data": "category.name" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <a href="/Admin/Product/CreateUpdate?id=${data}"><i class="bi bi-pencil-square"></i></a>
+                        <a onclick=removeProduct("/Admin/Product/Delete/${data}")><i class="bi bi-pencil-trash"></i></a>
+                    `
+                }
+            },
+        ]
+    })
+})
+
+function removeProduct(url) {
+    Swal.fire({
+        title: "Sure ?",
+        text: "Deleted items can't be reverted",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: '#d33',
+        confirmButtonText: "Delete!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+        }
+    })
+}
